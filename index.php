190ae13cdd9b10ba395cf2cfb58c0118c9ba9a6f
Axel Gromat <axel.gromat@gmail.com>
2021-05-06 13:41:43 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Livesearch</title>
+</head>
+
+<body>
+    <h1> Live Search de l'index des élèves de la promo 2021 de Développeur Web Fullstack </h1>
+    <form action="search.php" method="post">
+        <input type="text" name="name" id="name" placeholder="Search by name here ...">
+    </form>
+    <ul id="data-list">
+        <li id="data-item1"> Eleve 1</li>
+        <li id="data-item2"> Eleve 2</li>
+        <li id="data-item3"> Eleve 3</li>
+    </ul>
+</body>
+
+</html>
